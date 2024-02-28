aca2a7a165bc0594d62735db8ad84545fb8418d5
Filonych <e.filonych@gmail.com>
2024-02-28 19:45:31 +0300

    .

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
-
-const start = () => {
+const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_CONNECT)
     app.listen(PORT, () => console.log(`Server started on port - ${PORT}`));
   } catch (e) {
     console.log(e);
