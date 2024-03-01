bdb2069ca375bc6658f8b575c91e96fed13fc81e
Filonych <e.filonych@gmail.com>
2024-03-01 14:32:47 +0300

    .

--- server/controllers/todosController.js
+++ server/controllers/todosController.js
@@ -3,6 +3,13 @@ const TodosModel = require("../models/TodosModel");
 class TodosController {
   async getTodos(req, res) {
     try {
+        TodosModel.find({}, "title", (error, result) => {
+            if (error) {
+                res.status(400).json({ message: error })
+            }
+            
+            res.status(200).json({ todos: result })
+        })
         
     } catch (e) {
       res.status(400).json({ message: "Произошла ошибка при получении" });
@@ -26,6 +33,17 @@ class TodosController {
 
   async deleteTodo(req, res) {
     try {
+        if(!req.body.title) {
+            res.status(400).json({ message: "Пожалуйста, укажите заголовок" }) 
+        }
+
+        const { deletedCount } = await TodosModel.deleteOne({ title: req.body.title })
+
+        if (deletedCount === 0) {
+            res.status(400).json({ message: "Удаление не произошло, пожалуйста, проверьте заголовок" });
+        }
+        res.status(200).json({ message: "Элемент успешно удален" });
+
     } catch (e) {
       res.status(400).json({ message: "Произошла ошибка при удалении" });
     }
