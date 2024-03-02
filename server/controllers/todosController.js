2b816faf5fbd0ec3b061cc6f19dfc2f0f2150a3b
Filonych <e.filonych@gmail.com>
2024-03-02 13:19:53 +0300

    add backend for editTodo

--- server/controllers/todosController.js
+++ server/controllers/todosController.js
@@ -3,23 +3,23 @@ const TodosModel = require("../models/TodosModel");
 class TodosController {
   async getTodos(req, res) {
     try {
-        const result = await TodosModel.find({}, "title");
-        res.status(200).json({ todos: result });
+      const result = await TodosModel.find({}, "title");
+      res.status(200).json({ todos: result });
     } catch (error) {
-        res.status(400).json({ message: "Произошла ошибка при получении" });
+      res.status(400).json({ message: "Произошла ошибка при получении" });
     }
   }
 
   async addTodo(req, res) {
     try {
-        if(!req.body.title) {
-            res.status(400).json({ message: "Пожалуйста, добавьте заголовок" }) 
-        }
+      if (!req.body.title) {
+        res.status(400).json({ message: "Пожалуйста, добавьте заголовок" });
+      }
 
-        const TodoModel = new TodosModel({title: req.body.title})
+      const TodoModel = new TodosModel({ title: req.body.title });
 
-        await TodoModel.save()
-        res.status(200).json({ message: "Элемент успешно добавлен" });
+      await TodoModel.save();
+      res.status(200).json({ message: "Элемент успешно добавлен" });
     } catch (e) {
       res.status(400).json({ message: "Произошла ошибка при добавлении" });
     }
@@ -27,21 +27,48 @@ class TodosController {
 
   async deleteTodo(req, res) {
     try {
-        if(!req.body.title) {
-            res.status(400).json({ message: "Пожалуйста, укажите заголовок" }) 
-            return
-        }
+      if (!req.body.title) {
+        res.status(400).json({ message: "Пожалуйста, укажите заголовок" });
+        return;
+      }
 
-        const { deletedCount } = await TodosModel.deleteOne({ title: req.body.title })
+      const { deletedCount } = await TodosModel.deleteOne({
+        title: req.body.title,
+      });
 
-        if (deletedCount === 0) {
-            res.status(400).json({ message: "Удаление не произошло, пожалуйста, проверьте заголовок" });
-            return
-        }
-        res.status(200).json({ message: "Элемент успешно удален" });
-
+      if (deletedCount === 0) {
+        res
+          .status(400)
+          .json({
+            message: "Удаление не произошло, пожалуйста, проверьте заголовок",
+          });
+        return;
+      }
+      res.status(200).json({ message: "Элемент успешно удален" });
     } catch (e) {
       res.status(400).json({ message: "Произошла ошибка при удалении" });
+    }
+  }
+
+  async editTodo(req, res) {
+    try {
+      const updatedTodo = await TodosModel.findByIdAndUpdate(
+        req.body.title,
+        { title: req.body.title },
+        { new: true }
+      );
+
+      if (!updatedTodo) {
+        return res
+          .status(400)
+          .json({ message: "Произошла ошибка при редактировании" });
+      }
+
+      res
+        .status(200)
+        .json({ todo: updatedTodo, message: "Элемент успешно отредактирован" });
+    } catch (e) {
+      res.status(400).json({ message: "Произошла ошибка при редактировании" });
     }
   }
 }
